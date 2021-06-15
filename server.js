8840d43efa9acb34bb25c62b81910ff18b0e1424
igorpenedos <59774723+igorpenedos@users.noreply.github.com>
2021-06-15 16:18:10 -0400

    heroku

--- server.js
+++ server.js
@@ -16,9 +16,12 @@ app.get('/getData', (req, res) => {
     .then(response => res.send(response));
 })
 
-if (process.env.NODE_ENV === 'production'){
-    app.use(express.static('comic/build'))
-}
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 
